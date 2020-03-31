d7c5410bf597f09bee73054064e9b2cc5ba600ea
Matt Travi <programmer@travi.org>
2020-03-31 00:46:57 -0500

    feat(definitions): added badge references

--- src/plugin-test.js
+++ src/plugin-test.js
@@ -7,32 +7,46 @@ import * as zoneMutator from './zone-mutator';
 import plugin from './plugin';
 
 suite('plugin', () => {
-  let sandbox;
-  const node = any.simpleObject();
+  let sandbox, node;
 
   setup(() => {
     sandbox = sinon.createSandbox();
 
     sandbox.stub(zone, 'default');
     sandbox.stub(zoneMutator, 'default');
+
+    node = {...any.simpleObject(), children: []};
   });
 
   teardown(() => sandbox.restore());
 
   test('that the badges are injected into the appropriate zones', () => {
     const badgeGroupNames = any.listOf(any.word);
-    const badgeGroups = badgeGroupNames.map(() => any.simpleObject());
+    const badgeGroups = badgeGroupNames.map(() => any.objectWithKeys(
+      any.listOf(any.word),
+      {factory: () => ({link: any.url(), img: any.url()})}
+    ));
     const zoneMutators = badgeGroupNames.map(() => () => undefined);
     const transformer = plugin(Object.fromEntries(zip(badgeGroupNames, badgeGroups)));
     zip(badgeGroups, zoneMutators).forEach(([group, mutator]) => {
       zoneMutator.default.withArgs(group).returns(mutator);
     });
+    const flattenedBadgeDetails = Object.values(badgeGroups).reduce((acc, badgeGroup) => ({...acc, ...badgeGroup}), {});
 
     transformer(node);
 
     assert.callCount(zoneMutator.default, badgeGroupNames.length);
     zip(badgeGroupNames, zoneMutators).forEach(([groupName, mutator]) => {
       assert.calledWith(zone.default, node, `${groupName}-badges`, mutator);
+    });
+    Object.entries(flattenedBadgeDetails).forEach(([badgeName, badgeDetails]) => {
+      const linkDefinition = node.children.find(child => `${badgeName}-link` === child.label);
+      const imgDefinition = node.children.find(child => `${badgeName}-badge` === child.label);
+
+      assert.equal(linkDefinition.url, badgeDetails.link);
+      assert.equal(linkDefinition.type, 'definition');
+      assert.equal(imgDefinition.url, badgeDetails.img);
+      assert.equal(imgDefinition.type, 'definition');
     });
   });
 
--- src/plugin.js
+++ src/plugin.js
@@ -6,6 +6,11 @@ export default function (badges) {
     if (badges) {
       Object.entries(badges).forEach(([groupName, group]) => {
         zone(node, `${groupName}-badges`, getZoneMutator(group));
+
+        Object.entries(group).forEach(([badgeName, badgeDetails]) => {
+          node.children.push({type: 'definition', label: `${badgeName}-link`, url: badgeDetails.link});
+          node.children.push({type: 'definition', label: `${badgeName}-badge`, url: badgeDetails.img});
+        });
       });
     }
   };
