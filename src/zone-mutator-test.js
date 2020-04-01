002662fddaecbf38e1026f52423ba2c38b2ec2dc
Matt Travi <programmer@travi.org>
2020-04-01 01:00:07 -0500

    fix: listed one badge per line

--- src/zone-mutator-test.js
+++ src/zone-mutator-test.js
@@ -11,6 +11,10 @@ suite('zone mutator', () => {
   const detailsOfBadges = any.simpleObject();
   const detailsOfBadgesEntries = Object.entries(detailsOfBadges);
   const linkReferences = detailsOfBadgesEntries.map(() => any.simpleObject());
+  const linkReferencesSeparatedByNewLines = linkReferences.reduce(
+    (acc, reference) => acc.concat({type: 'text', value: '\n'}, reference),
+    []
+  );
 
   setup(() => {
     sandbox = sinon.createSandbox();
@@ -27,7 +31,7 @@ suite('zone mutator', () => {
   test('that the badges are added to the empty zone', () => {
     assert.deepEqual(
       mutateZone(detailsOfBadges)(start, [], end),
-      [start, {type: 'paragraph', children: linkReferences}, end]
+      [start, {type: 'paragraph', children: linkReferencesSeparatedByNewLines.slice(1)}, end]
     );
   });
 
@@ -36,7 +40,17 @@ suite('zone mutator', () => {
 
     assert.deepEqual(
       mutateZone(detailsOfBadges)(start, [{type: 'paragraph', children: existingLinkReferences}], end),
-      [start, {type: 'paragraph', children: [...existingLinkReferences, ...linkReferences]}, end]
+      [
+        start,
+        {
+          type: 'paragraph',
+          children: [
+            ...existingLinkReferences,
+            ...linkReferencesSeparatedByNewLines
+          ]
+        },
+        end
+      ]
     );
   });
 });
