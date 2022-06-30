a36a83b790698a5ad96a379902681b77057572f8
Matt Travi <developer@travi.org>
2022-06-30 16:11:09 -0500

    fix(dual-mode): made the es bundle an `.mjs` file so node treats it properly

--- example.js
+++ example.js
@@ -3,7 +3,7 @@
 import stubbedFs from 'mock-fs';
 import fs from 'fs';
 import remark from 'remark';
-import injectBadges from './lib/index.cjs';
+import injectBadges from './lib/index.js';
 
 // remark-usage-ignore-next
 stubbedFs();
